c7afbd856550a4dd281372d1a1d2a9cd8c11b99a
unknown <evikody06@gmail.com>
2025-04-27 02:08:55 +0800

    Input the right symbol for self and a method to get the current channel

--- TV/TV.py
+++ TV/TV.py
@@ -1,14 +1,20 @@
 class TV:
     # Constructor
     def __init__self(self):
-        self_channel = 1 # Start the TV Channel on channel 1
-        self_volume_level = 1 # Start the TV volume level with level 1
+        self.channel = 1 # Start the TV Channel on channel 1
+        self.volumeLevel = 1 # Start the TV volume level with level 1
         self.on = False # TV is initially turned
 
 def turn_on(self):
+    # Method to turn the TV on
     self.on = True
 
 def turn_off(self):
-    self_off = True
+    # Method to turn the TV off
+    self.off = True
 
+def get_channel(self):
+    # Method to get the current channel
+    return self.channel()
 
+def set_channel(self, channel):
