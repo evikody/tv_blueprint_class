eb342788ac7f175a427fd0f60dc103574923c2f7
unknown <evikody06@gmail.com>
2025-04-27 02:29:09 +0800

    Methods for volume and channel

--- TV/TV.py
+++ TV/TV.py
@@ -3,7 +3,7 @@ class TV:
     def __init__self(self):
         self.channel = 1 # Start the TV Channel on channel 1
         self.volumeLevel = 1 # Start the TV volume level with level 1
-        self.on = False # TV is initially turned
+        self.on = False # TV is initially turned off
 
 def turn_on(self):
     # Method to turn the TV on
@@ -18,3 +18,33 @@ def get_channel(self):
     return self.channel()
 
 def set_channel(self, channel):
+    # Method to set the TV to a new channel
+    if self.on and 1 <= channel <= 120: # Only set if the TV is on and the channel is between 1 to 120
+        self.channel = channel
+
+def get_volume(self):
+    # Method to get the volume level
+    return self.volumeLevel
+
+def set_volume(self, volumeLevel):
+    # Method to get set the TV to a new volume level
+    if self.on and 1 <= volumeLevel <= 7: # Only set if the TV is on and the volume level is  between 1 to 7
+        self.volumeLevel = volumeLevel
+
+def channel_up(self):
+    # Method to increase the channel by 1
+    if self.on and self.channel < 120: # Only increase the channel if the TV is on and the channel is below 120
+        self.channel += 1
+
+def channel_down(self):
+    # Method to decrease the channel by 1
+    if self.on and self.channel > 1: # Only decrease the channel if the TV is on and the channel is greater than 1
+        self.channel -= 1
+
+def volume_up(self):
+    if self.on and self.volumeLevel < 7:
+        self.volumeLevel += 1
+
+def volume_down(self):
+    if self.on and self.volumeLevel > 1:
+        self.volumeLevel -= 1
