e56738c73f21fc8f810386a4970044c374162354
unknown <evikody06@gmail.com>
2025-04-27 02:02:37 +0800

    A function for turning on and off the TV

--- TV/TV.py
+++ TV/TV.py
@@ -3,4 +3,12 @@ class TV:
     def __init__self(self):
         self_channel = 1 # Start the TV Channel on channel 1
         self_volume_level = 1 # Start the TV volume level with level 1
-        self.on = False # TV is initially turned off
+        self.on = False # TV is initially turned
+
+def turn_on(self):
+    self.on = True
+
+def turn_off(self):
+    self_off = True
+
+
