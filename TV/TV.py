f9a6ab79cc04092c8b15e33947a2691957e972b4
unknown <evikody06@gmail.com>
2025-04-27 01:58:28 +0800

    Sets a constructor for a class named TV

---
+++ TV/TV.py
@@ -0,0 +1,6 @@
+class TV:
+    # Constructor
+    def __init__self(self):
+        self_channel = 1 # Start the TV Channel on channel 1
+        self_volume_level = 1 # Start the TV volume level with level 1
+        self.on = False # TV is initially turned off
